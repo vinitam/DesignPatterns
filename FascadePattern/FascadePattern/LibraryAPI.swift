bde4ee5b3bcb871e24193e9b64adcb126ea2aace
Vinita <vinita.miranda@rksv.in>
2017-02-03 15:36:07 +0530

    Initial commit.

---
+++ FascadePattern/FascadePattern/LibraryAPI.swift
@@ -0,0 +1,9 @@
+//
+//  LibraryAPI.swift
+//  FascadePattern
+//
+//  Created by Vinita on 2/3/17.
+//
+//
+
+import Foundation
---
+++ FascadePattern/FascadePattern/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FascadePattern
+//
+//  Created by Vinita on 2/3/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
